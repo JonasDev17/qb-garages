9f4679db291571abf3bd5b369bff3433fa318aa7
LoveFistRP <qbcore@programmer.net>
2021-04-18 15:32:34 -0700

    Moved callback

--- server/main.lua
+++ server/main.lua
@@ -15,6 +15,19 @@ AddEventHandler('qb-garages:server:UpdateOutsideVehicles', function(Vehicles)
     OutsideVehicles[CitizenId] = Vehicles
 end)
 
+QBCore.Functions.CreateCallback("qb-garage:server:checkVehicleOwner", function(source, cb, plate)
+    local src = source
+    local pData = QBCore.Functions.GetPlayer(src)
+
+    exports['ghmattimysql']:execute('SELECT * FROM player_vehicles WHERE plate = @plate AND citizenid = @citizenid', {['@plate'] = plate, ['@citizenid'] = pData.PlayerData.citizenid}, function(result)
+        if result[1] ~= nil then
+            cb(true)
+        else
+            cb(false)
+        end
+    end)
+end)
+
 QBCore.Functions.CreateCallback("qb-garage:server:GetOutsideVehicles", function(source, cb)
     local Ply = QBCore.Functions.GetPlayer(source)
     local CitizenId = Ply.PlayerData.citizenid

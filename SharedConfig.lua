cf0639b04c8f67b857f841279da957c45468f8d3
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2021-08-27 20:43:37 -0500

    Revert "Show/Hide Blips based on config settings"

--- SharedConfig.lua
+++ SharedConfig.lua
@@ -5,98 +5,84 @@ Garages = {
         spawnPoint = vector4(270.94, -342.96, 43.97, 161.5),
         putVehicle = vector3(276.69, -339.85, 44.91),
         isHouse = false,
-        showBlip = true
     },
     ["sapcounsel"] = {
         label = "San Andreas Parking",
         takeVehicle = vector3(-330.01, -780.33, 33.96),
         spawnPoint = vector4(-334.44, -780.75, 33.96, 137.5),
         putVehicle = vector3(-336.31, -774.93, 33.96),
-        showBlip = true,
     },
     ["spanishave"] = {
         label = "Spanish Ave Parking",
         takeVehicle = vector3(-1160.86, -741.41, 19.63),
         spawnPoint = vector4(-1163.88, -749.32, 18.42, 35.5),
         putVehicle = vector3(-1147.58, -738.11, 19.31),
-        showBlip = true,
     },
     ["caears24"] = {
         label = "Caears 24 Parking",
         takeVehicle = vector3(69.84, 12.6, 68.96),
         spawnPoint = vector4(73.21, 10.72, 68.83, 163.5),
         putVehicle = vector3(65.43, 21.19, 69.47),
-        showBlip = true,
     },
     ["caears242"] = {
         label = "Caears 24 Parking",
         takeVehicle = vector3(-475.31, -818.73, 30.46),
         spawnPoint = vector4(-472.03, -815.47, 30.5, 177.5),
         putVehicle = vector3(-453.6, -817.08, 30.61),
-        showBlip = true,
     },
     ["lagunapi"] = {
         label = "Laguna Parking",
         takeVehicle = vector3(364.37, 297.83, 103.49),
         spawnPoint = vector4(367.49, 297.71, 103.43, 340.5),
         putVehicle = vector3(363.04, 283.51, 103.38),
-        showBlip = true,
     },
     ["airportp"] = {
         label = "Airport Parking",
         takeVehicle = vector3(-796.86, -2024.85, 8.88),
         spawnPoint = vector4(-800.41, -2016.53, 9.32, 48.5),
         putVehicle = vector3(-804.84, -2023.21, 9.16),
-        showBlip = true,
     },
     ["beachp"] = {
         label = "Beach Parking",
         takeVehicle = vector3(-1183.1, -1511.11, 4.36),
         spawnPoint = vector4(-1181.0, -1505.98, 4.37, 214.5),
         putVehicle = vector3(-1176.81, -1498.63, 4.37),
-        showBlip = true,
     },
     ["themotorhotel"] = {
         label = "The Motor Hotel Parking",
         takeVehicle = vector3(1137.77, 2663.54, 37.9),            
         spawnPoint = vector4(1137.69, 2673.61, 37.9, 359.5),      
         putVehicle = vector3(1137.75, 2652.95, 37.9),
-        showBlip = true,
     },
     ["liqourparking"] = {
         label = "Liqour Parking",
         takeVehicle = vector3(934.95, 3606.59, 32.81),
         spawnPoint = vector4(941.57, 3619.99, 32.5, 141.5),
         putVehicle = vector3(939.37, 3612.25, 32.69),
-        showBlip = true,
     },
     ["shoreparking"] = {
         label = "Shore Parking",
         takeVehicle = vector3(1726.21, 3707.16, 34.17),
         spawnPoint = vector4(1730.31, 3711.07, 34.2, 20.5),
         putVehicle = vector3(1737.13, 3718.91, 34.04),
-        showBlip = true,
     },
     ["haanparking"] = {
         label = "Bell Farms Parking",
         takeVehicle = vector3(78.34, 6418.74, 31.28),
         spawnPoint = vector4(70.71, 6425.16, 30.92, 68.5), 
         putVehicle = vector3(85.3, 6427.52, 31.33),
-        showBlip = true,
     },
     ["dumbogarage"] = {
         label = "Dumbo Private Parking",
         takeVehicle = vector3(157.26, -3240.00, 7.00),
         spawnPoint = vector4(165.32, -3236.10, 5.93, 268.5), 
         putVehicle = vector3(165.32, -3230.00, 5.93),
-        showBlip = true,
     },
     ["pillboxgarage"] = {
         label = "Pillbox Garage Parking",
         takeVehicle = vector3(215.9499, -809.698, 30.731),
         spawnPoint = vector4(234.1942, -787.066, 30.193, 159.6),
         putVehicle = vector3(218.0894, -781.370, 30.389),
-        showBlip = true,
     },
 }
 
@@ -141,7 +127,6 @@ GangGarages = {
 Depots = {
     ["hayesdepot"] = {
         label = "Hayes Depot",
-        takeVehicle = vector4(491.0, -1314.69, 29.25, 304.5),
-        showBlip = true
+        takeVehicle = vector4(491.0, -1314.69, 29.25, 304.5)
     }
 }

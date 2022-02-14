2d585abaf1a5ac9b462c6126fcfebf5ac6ba42a8
Sna-aaa <88613496+Sna-aaa@users.noreply.github.com>
2022-02-14 15:00:45 +0100

    Bug correction + config value
    Bug : In shared garages when you take someone else car and put it back, it goes to the impound. This is resolved
    
    New configuration for shared garages. Set to false by default, you can choose if you want or not the shared garages for Job and Gang

--- config.lua
+++ config.lua
@@ -1,4 +1,5 @@
 AutoRespawn = false --True == auto respawn cars that are outside into your garage on script restart, false == does not put them into your garage and players have to go to the impound
+SharedGarages = false   --True == Gang and job garages are shared, false == Gang and Job garages are personal
 
 Garages = {
     ["motelgarage"] = {

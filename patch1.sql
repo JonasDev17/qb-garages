f3eec380e98a68542207733ac8301aa133248859
Jonas Suter <jonas.suter@fhnw.ch>
2022-06-17 20:08:21 +0200

    Update patch1.sql

--- patch1.sql
+++ patch1.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `player_vehicles`
-ADD COLUMN `parkingspot` VARCHAR(50) NULL DEFAULT NULL AFTER `garage`;
+ADD COLUMN `parkingspot` VARCHAR(50) NULL DEFAULT NULL AFTER `garage`,
 ADD COLUMN `damage` VARCHAR(1500) NULL DEFAULT NULL AFTER `parkingspot`;
